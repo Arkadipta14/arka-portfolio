60c88a117bc8d80c494cd6346e04563c618ff39c
Arkadipta14 <arkadiptapaul2@gmail.com>
2026-06-16 22:13:22 +0530

    additon of another project

--- src/data/projects.js
+++ src/data/projects.js
@@ -14,7 +14,21 @@ const projects = [
     link: "#"
   },
   {
-    id: "p2",
+  id: "p2",
+  title: "Demand IQ",
+  short: "Built an end-to-end demand forecasting platform for medical products, leveraging scalable ETL pipelines, time-series forecasting, and anomaly detection to optimise inventory planning, reduce stockouts, and improve supply chain decision-making.",
+  tech: ["Python","SQL","Apache Spark","ETL Pipelines","Prophet","AWS","Azure"],
+  details: [
+    "Improved inventory planning efficiency by 40% and reduced stockout incidents by 30%",
+    "Engineered bronze → silver → gold ETL pipelines, reducing data processing time by 45% and improving data quality by 35%",
+    "Developed Prophet-based time-series forecasting models, increasing demand prediction accuracy by 50%",
+    "Built anomaly detection systems for stockpiling and drug diversion, reducing suspicious transactions by 25% and improving fraud detection efficiency by 40%",
+    "Performed exploratory data analysis to identify demand and regional trends, enabling 20% faster business decision-making"
+  ],
+  link: "#"
+  },
+  {
+    id: "p3",
     title: "Knowledge IQ",
     short: "Built a document and web content ingestion system with vectorisation for efficient Q&A and summarisation,enabling faster and more accurate information retrieval with source traceability.",
     tech: ["Python", "Flask", "Langchain","Azure","FAISS","Computer Vision"],
